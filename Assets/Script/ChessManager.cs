b5396f923dda3f68442c253718d4f9eba75d73b9
Mary <phunmung@gmail.com>
2020-11-14 01:19:07 +0900

    Chess Piece Sorting, ChessBoard, Particle System Update

--- Assets/Script/ChessManager.cs
+++ Assets/Script/ChessManager.cs
@@ -12,6 +12,8 @@ public class ChessManager : MonoBehaviour
     private GameObject[] bPawns, bKnights, bRooks, bBishops;
     private GameObject wKing, wQueen, bKing, bQueen;
 
+    private List<GameObject> whitePieces, blackPieces;
+
     private void Awake()
     {
         wPawns = new GameObject[8];
@@ -23,6 +25,9 @@ public class ChessManager : MonoBehaviour
         bKnights = new GameObject[2];
         bRooks = new GameObject[2];
         bBishops = new GameObject[2];
+
+        whitePieces = new List<GameObject>();
+        blackPieces = new List<GameObject>();
     }
 
     public IEnumerator InitiateChess()
@@ -32,6 +37,10 @@ public class ChessManager : MonoBehaviour
         {
             wPawns[i] = Instantiate(wPawnPrefab, new Vector2(0f, -6f), Quaternion.identity);
             bPawns[i] = Instantiate(bPawnPrefab, new Vector2(0f, 6f), Quaternion.identity);
+
+            // Listing
+            whitePieces.Add(wPawns[i]);
+            blackPieces.Add(bPawns[i]);
         }
 
         // Instantiate Rook, Knight and Bishop
@@ -44,6 +53,14 @@ public class ChessManager : MonoBehaviour
             bRooks[i] = Instantiate(bRookPrefab, new Vector2(0f, 6f), Quaternion.identity);
             bKnights[i] = Instantiate(bKnightPrefab, new Vector2(0f, 6f), Quaternion.identity);
             bBishops[i] = Instantiate(bBishopPrefab, new Vector2(0f, 6f), Quaternion.identity);
+
+            // Listing
+            whitePieces.Add(wRooks[i]);
+            whitePieces.Add(wKnights[i]);
+            whitePieces.Add(wBishops[i]);
+            blackPieces.Add(bRooks[i]);
+            blackPieces.Add(bKnights[i]);
+            blackPieces.Add(bBishops[i]);
         }
 
         // Instantiate Queen and King
@@ -51,6 +68,12 @@ public class ChessManager : MonoBehaviour
         wKing = Instantiate(wKingPRefab, new Vector2(0f, -6f), Quaternion.identity);
         bQueen = Instantiate(bQueenPrefab, new Vector2(0f, 6f), Quaternion.identity);
         bKing = Instantiate(bKingPRefab, new Vector2(0f, 6f), Quaternion.identity);
+
+        // Listing
+        whitePieces.Add(wQueen);
+        whitePieces.Add(wKing);
+        blackPieces.Add(bQueen);
+        blackPieces.Add(bKing);
 
         // Move all pawn to the correct position
         for (int i = -4; i < 4; i++)
@@ -95,5 +118,17 @@ public class ChessManager : MonoBehaviour
         wKing.transform.DOMove(new Vector2(0.5f, -3.5f), 0.5f, false);
         bQueen.transform.DOMove(new Vector2(-0.5f, 3.5f), 0.5f, false);
         bKing.transform.DOMove(new Vector2(0.5f, 3.5f), 0.5f, false);
+        
+        yield return new WaitForSeconds(0.51f);
+        
+        // Update it's rendering order. This function should always get called when you move the piece
+        foreach (GameObject pieces in whitePieces) 
+        {
+            pieces.GetComponent<ChessPieceProperties>().UpdateRenderOrder();
+        }
+        foreach (GameObject pieces in blackPieces)
+        {
+            pieces.GetComponent<ChessPieceProperties>().UpdateRenderOrder();
+        }
     }
 }
