3e2b0a2f7a0e3407216446563c0cfddc1358eab3
Mary <phunmung@gmail.com>
2020-11-13 21:55:23 +0900

    Cat

---
+++ Assets/Script/ChessManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ChessManager : MonoBehaviour
+{
+    [SerializeField] private GameObject wPawnPrefab, wKnightPrefab, wRookPrefab, wBishopPrefab, wQueenPrefab, wKingPRefab;
+    [SerializeField] private GameObject bPawnPrefab, bKnightPrefab, bRookPrefab, bBishopPrefab, bQueenPrefab, bKingPRefab;
+
+    private GameObject[] wPawns, wKnights, wRooks, wBishops;
+    private GameObject[] bPawns, bKnights, bRooks, bBishops;
+    private GameObject wKing, wQueen, bKing, bQueen;
+
+    private void Awake()
+    {
+        wPawns = new GameObject[8];
+        wKnights = new GameObject[2];
+        wRooks = new GameObject[2];
+        wBishops = new GameObject[2];
+
+        bPawns = new GameObject[8];
+        bKnights = new GameObject[2];
+        bRooks = new GameObject[2];
+        bBishops = new GameObject[2];
+    }
+
+    public IEnumerator InitiateChess()
+    {
+        // Instantiate pawn
+        for (int i = 0; i < 8; i++)
+        {
+            wPawns[i] = Instantiate(wPawnPrefab, new Vector2(0f, -6f), Quaternion.identity);
+            bPawns[i] = Instantiate(bPawnPrefab, new Vector2(0f, 6f), Quaternion.identity);
+        }
+
+        // Instantiate Rook, Knight and Bishop
+        for (int i = 0; i < 2; i++)
+        {
+            wRooks[i] = Instantiate(wRookPrefab, new Vector2(0f, -6f), Quaternion.identity);
+            wKnights[i] = Instantiate(wKnightPrefab, new Vector2(0f, -6f), Quaternion.identity);
+            wBishops[i] = Instantiate(wBishopPrefab, new Vector2(0f, -6f), Quaternion.identity);
+
+            bRooks[i] = Instantiate(bRookPrefab, new Vector2(0f, 6f), Quaternion.identity);
+            bKnights[i] = Instantiate(bKnightPrefab, new Vector2(0f, 6f), Quaternion.identity);
+            bBishops[i] = Instantiate(bBishopPrefab, new Vector2(0f, 6f), Quaternion.identity);
+        }
+
+        // Instantiate Queen and King
+        wQueen = Instantiate(wQueenPrefab, new Vector2(0f, -6f), Quaternion.identity);
+        wKing = Instantiate(wKingPRefab, new Vector2(0f, -6f), Quaternion.identity);
+        bQueen = Instantiate(bQueenPrefab, new Vector2(0f, 6f), Quaternion.identity);
+        bKing = Instantiate(bKingPRefab, new Vector2(0f, 6f), Quaternion.identity);
+
+        // Move all pawn to the correct position
+        for (int i = -4; i < 4; i++)
+        {
+            wPawns[i + 4].transform.DOMove(new Vector2(i + 0.5f, -2.5f), 0.5f, false);
+            bPawns[i + 4].transform.DOMove(new Vector2((i * -1) - 0.5f, 2.5f), 0.5f, false);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        // Move all Rooks to the correct position
+        for (int i = 0; i < 2; i++)
+        {
+            float degree = Mathf.Deg2Rad * (90 + i * 180);
+            wRooks[i].transform.DOMove(new Vector2(Mathf.Sin(degree) * 3.5f, -3.5f), 0.5f, false);
+            bRooks[i].transform.DOMove(new Vector2(Mathf.Sin(degree) * 3.5f, 3.5f), 0.5f, false);
+        }
+        
+        yield return new WaitForSeconds(0.5f);
+
+        // Move all Kights to the correct position
+        for (int i = 0; i < 2; i++)
+        {
+            float degree = Mathf.Deg2Rad * (90 + i * 180);
+            wKnights[i].transform.DOMove(new Vector2(Mathf.Sin(degree) * 2.5f, -3.5f), 0.5f, false);
+            bKnights[i].transform.DOMove(new Vector2(Mathf.Sin(degree) * 2.5f, 3.5f), 0.5f, false);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        // Move all Bishops to the correct position
+        for (int i = 0; i < 2; i++)
+        {
+            float degree = Mathf.Deg2Rad * (90 + i * 180);
+            wBishops[i].transform.DOMove(new Vector2(Mathf.Sin(degree) * 1.5f, -3.5f), 0.5f, false);
+            bBishops[i].transform.DOMove(new Vector2(Mathf.Sin(degree) * 1.5f, 3.5f), 0.5f, false);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+        
+        // Move King and Queen to the correct position
+        wQueen.transform.DOMove(new Vector2(-0.5f, -3.5f), 0.5f, false);
+        wKing.transform.DOMove(new Vector2(0.5f, -3.5f), 0.5f, false);
+        bQueen.transform.DOMove(new Vector2(-0.5f, 3.5f), 0.5f, false);
+        bKing.transform.DOMove(new Vector2(0.5f, 3.5f), 0.5f, false);
+    }
+}
---
+++ Assets/Script/GameStateManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameStateManager : MonoBehaviour
+{
+    // Reference
+    [SerializeField] GameObject chessBoard;
+    [SerializeField] ParticleSystem dust;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        StartCoroutine(ChessBoardInitiate(chessBoard, 15f, 1f, 0.5f, 1f));
+    }
+
+    private IEnumerator ChessBoardInitiate(GameObject board, float initialSize, float finalSize, float time, float speedUp)
+    {
+        var animator = chessBoard.GetComponent<Animator>();
+        animator.Play("Drop");
+
+        var clipInfo = animator.GetCurrentAnimatorClipInfo(0);
+        yield return new WaitForSeconds(clipInfo[0].clip.length);
+
+        StartCoroutine(Shake(chessBoard.transform, 0.25f, 0.25f));
+
+        for (int i = -8; i <= 8; i++)
+        {
+            for (int j = -8; j <= 8; j++)
+            {
+                var particle = Instantiate(dust, new Vector3(i * 0.5f, j * 0.5f, 0), Quaternion.identity);
+                Destroy(particle.gameObject, particle.GetComponent<ParticleSystem>().main.duration - 0.1f);
+            }
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        StartCoroutine(GetComponent<ChessManager>().InitiateChess());
+        chessBoard.GetComponentInChildren<CanvasGroup>().DOFade(1.0f, 3f);
+    }
+
+    private IEnumerator Shake(Transform target, float magnitude, float time)
+    {
+        float timeElapsed = 0.0f;
+        Vector3 originalPosition = target.position;
+
+        do
+        {
+            timeElapsed += Time.deltaTime;
+            target.position = originalPosition + new Vector3(Random.Range(-magnitude, +magnitude), 
+                Random.Range(-magnitude, +magnitude), Random.Range(-magnitude, +magnitude));
+            yield return null;
+        } while (timeElapsed < time);
+
+        target.position = originalPosition;
+    }
+}
